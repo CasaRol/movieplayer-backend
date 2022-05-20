788da765e331d305d577b69eca26b27b1dcc0c7c
Pottemuld <sdowning@live.dk>
2022-05-20 09:57:57 +0200

    Add json file as database

--- app.js
+++ app.js
@@ -1,17 +1,45 @@
-var express = require('express');
+const fs = require('fs')
+const express = require('express');
+const { Console } = require('console');
 var app = express();
+
 const PORT = 3001;
+
+var db = 
 
 app.listen(PORT, () => {
     console.log("Server running on port " + PORT)
 })
 
 // Example request
-app.get('/url', (req, res, next) => {
+app.get('/url', (req, res) => {
     res.json(['Tony', 'Lisa', 'Michael', 'Ginger', 'Food']);
 });
 
 
-app.get('/testvideo', (req, res, next) => {
-    res.sendFile('assets/Test_video1.mp4', {root: __dirname})
+app.get('/testvideo', (req, res) => {
+    var videoName = req.query.videoName;
+    var videoPath = null;
+
+    var file = fs.readFileSync('./db.json');
+    var data = JSON.parse(file);
+    
+
+    var videos = data["videos"];
+
+    for (var element in videos) {
+        if (videos[element]["name"] == videoName) {
+            videoPath = videos[element]["path"]
+            break;
+        }
+    }
+
+    if (videoPath != null) {
+        res.sendFile(videoPath, {root: __dirname})
+    }
+    else {
+        res.status(404).send("Video could not be located")
+        
+    }
+    
 })
